0bdf7743bd8da090a3f14900f159726634c61d7e
sweg-db <bhd03067@naver.com>
2022-01-05 21:57:19 +0900

    0105
    - 멤버 수정 삭제 예시

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,26 +4,35 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
     public static void main(String[] args) {
         //데이터 베이스와 연결이 됨                                          persistence.xml적은 unit과 이름 맞춤
-        EntityManagerFactory enf = Persistence.createEntityManagerFactory("jpa-practice");
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpa-practice");
         //그다음 메니저를 꺼내야함
-        EntityManager em = enf.createEntityManager();
+        EntityManager em = emf.createEntityManager();
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
         //코드 작성
-        Member member = new Member();
-        member.setId(1L);
-        member.setName("HelloA");
-        em.persist(member);
+        try {
+            //<T> T find(Class<T> var1, Object var2);
+//            Member findMember = em.find(Member.class, 1L);
+            List<Member> result = em.createQuery("select m from Member as m", Member.class)
+                            .setFirstResult(1)
+                            .setMaxResults(2)
+                            .getResultList();
+            for (Member member:result){
+                System.out.println("NAME = " + member.getName());
+            }
 
-        tx.commit();
-
-        //애플리케이션이 끝나면 managerfactory를 닫아줘야함
-        em.close();
-        enf.close();
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback(); //예외가 생길 시 롤백해주고
+        } finally {
+            em.close(); //작업이 끝나면 엔티티 매니저를 닫아줌.
+        }
+        emf.close(); //전체 애플리케이션이 끝나면 팩토리도 닫아줌.
     }
 }
